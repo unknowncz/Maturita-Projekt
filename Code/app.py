e06ea36c03682e935a575fb474ff115b3af08811
Radexis <radek.junior.kladno@gmail.com>
2023-10-13 12:40:58 +0200

    Moje maminka je hodná
    pusuˇju velmi funekenčene

--- Code/app.py
+++ Code/app.py
@@ -1,8 +1,9 @@
-
 from flask import Flask, redirect, url_for, request, render_template
-
+import flask_login
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
 app = Flask(__name__)
+app.secret_key = 'super secret string'  # Change this!
 
 @app.route('/',methods = ["POST", "GET"])
 def home():
@@ -22,17 +23,74 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template("login.html")
+        '''
+                <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+                </form>
+                '''
+
+    email = request.form['email']
+    if email in users and Flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return Flask.redirect(Flask.url_for('protected'))
+
+    return 'Bad login'
+
 
 @app.route('/register')
 def register():
     return render_template("debug.html") #Sends the client to the void
 
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
 #@app.route('/salt',methods = ['POST', 'GET'])
 #def salt():
     ...
 
+#Flask Login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
+
+users = {'foo@bar.tld': {'password': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
 
 if __name__ == '__main__':
 
